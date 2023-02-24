3c01e5e81f08004e5c228e8954cf96d8d47f37df
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2023-02-24 14:38:51 +0100

    run PowerShell Beautifier

--- W10ANDW11-NETWORK-TCP-BACKSUBOPTIMIZATION.ps1
+++ W10ANDW11-NETWORK-TCP-BACKSUBOPTIMIZATION.ps1
@@ -15,7 +15,7 @@
 
 # PROMPT THE USER TO ELEVATE THE SCRIPT
 # Great thanks to "Karl Wester-Ebbinghaus/Karl-WE" for this very useful aid.
-if (-not (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+if (-not (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
   $arguments = "-NoExit -ExecutionPolicy Bypass -File `"$($myInvocation.MyCommand.Definition)`""
   Start-Process powershell -Verb runAs -ArgumentList $arguments
@@ -23,23 +23,23 @@ if (-not (New-Object Security.Principal.WindowsPrincipal([Security.Principal.Win
 }
 
 #SET ALL NIC ADVANCED SETTINGS TO DEFAULT
-Get-Netadapter -Physical | Reset-NetAdapterAdvancedProperty -DisplayName "*"
+Get-NetAdapter -Physical | Reset-NetAdapterAdvancedProperty -DisplayName "*"
 
 # SET RSS TO DEFAULT
-netsh int tcp set global RSS=Enabled
+netsh int tcp Set-Variable global RSS=Enabled
 Set-NetOffloadGlobalSetting -ReceiveSideScaling Enabled
 
 # SET RSC TO DEFAULT
-netsh int tcp set global RSC=Enabled
+netsh int tcp Set-Variable global RSC=Enabled
 Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing Enabled
 
 # SET PACKET COALESCING TO DEFAULT
 Set-NetOffloadGlobalSetting -PacketCoalescingFilter Enabled
 
 # SET TCP CONGESTION CONTROL TO DEFAULT
-netsh int tcp set supplemental template=Datacenter congestionprovider=CUBIC
-netsh int tcp set supplemental template=Datacentercustom congestionprovider=CUBIC
-netsh int tcp set global ECN=Disabled
+netsh int tcp Set-Variable supplemental template=Datacenter congestionprovider=CUBIC
+netsh int tcp Set-Variable supplemental template=Datacentercustom congestionprovider=CUBIC
+netsh int tcp Set-Variable global ECN=Disabled
 
 # SET TCP PROFILE TO DEFAULT (Internet)
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Nsi\{eb004a03-9b1a-11d4-9123-0050047759bc}\27\" -Name "06000000" -Value (([byte[]](0x00,0x00,0x00,0x00,0xff,0xff,0xff,0xff)))
@@ -48,18 +48,18 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Nsi\{eb004a03-9b1
 # SET TCPACKFREQUENCY TO DEFAULT
 $NICs = Get-NetAdapter -Physical | Select-Object DeviceID
 foreach ($adapter in $NICs)
-  {
-  $NICGUID = $adapter | Select-Object DeviceID | Select DeviceID -ExpandProperty DeviceID | Out-String -Stream
+{
+  $NICGUID = $adapter | Select-Object DeviceID | Select-Object DeviceID -ExpandProperty DeviceID | Out-String -Stream
   $REGKEYPATH = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\$NICGUID\" | Out-String -Stream
   Remove-ItemProperty -Path "$REGKEYPATH" -Name 'TcpAckFrequency'
-  }
+}
 
 
 # SET TCPDELAY TO DEFAULT
 $NICs = Get-NetAdapter -Physical | Select-Object DeviceID
 foreach ($adapter in $NICs)
-  {
-  $NICGUID = $adapter | Select-Object DeviceID | Select DeviceID -ExpandProperty DeviceID | Out-String -Stream
+{
+  $NICGUID = $adapter | Select-Object DeviceID | Select-Object DeviceID -ExpandProperty DeviceID | Out-String -Stream
   $REGKEYPATH = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\$NICGUID\" | Out-String -Stream
   Remove-ItemProperty -Path "$REGKEYPATH" -Name 'TcpNoDelay'
-  }
+}
